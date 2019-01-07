f0ed8557eaef6fedcf8885a01727477926c7da9d
Syns <jpo.drt@gmail.com>
2019-01-07 16:19:53 +0100

    ajout du js, ressources du mercredi

---
+++ index.js
@@ -0,0 +1,35 @@
+function popUp() {
+    reponse = prompt("Vient sur ce site trop cool !");
+        if (reponse == "oui") {
+            window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        } else if(reponse == "non"){
+            console.log("L'utilisateur a choisit "+reponse);
+        }
+        else {
+            alert("Rentre oui ou non !");
+            popUp();
+        }
+}
+
+setTimeout(popUp, 300000);
+
+
+document.querySelector('button').addEventListener('click', function() {
+    
+    document.body.style.backgroundColor = "magenta";
+    p = document.getElementsByTagName("p");
+
+    for (let i = 0; i < p.length; i++) {
+        p[i].style.color = "blue";
+        p[i].style.fontFamily = "papyrus";
+        
+    }
+    
+    head = document.getElementsByClassName("head");
+    
+    for (let i = 0; i < head.length; i++) {
+        head[i].style.color = "green";
+        head[i].style.fontFamily = "Comic Sans MS";
+        
+    }
+});
